62db63a89e27917af7e7473ff8fffe69eab80b83
Alexey Golubev <dr.freecx@gmail.com>
2025-03-25 23:25:22 +0300

    fix: grayscale jpg

--- src/format/jpg.rs
+++ src/format/jpg.rs
@@ -1,9 +1,12 @@
-use anyhow::anyhow;
+use anyhow::{anyhow, bail};
 use log::debug;
 use zune_jpeg::JpegDecoder;
 
 use super::Loader;
-use crate::image::{ColorType, Image};
+use crate::{
+    image::{ColorType, Image},
+    transform::grayscale_to_rgb,
+};
 
 pub struct Jpg;
 
@@ -12,18 +15,20 @@ impl Loader for Jpg {
         debug!("Use jpeg loader");
 
         let mut decoder = JpegDecoder::new(data);
-        let pixels = match decoder.decode() {
+        let mut pixels = match decoder.decode() {
             Ok(data) => data,
             Err(e) => return Err(anyhow!("{:?}", e)),
         };
         let info = decoder.info().unwrap();
 
-        Ok(Image {
-            data: pixels,
-            width: info.width as u32,
-            height: info.height as u32,
-            // TODO
-            color_type: ColorType::RGB8,
-        })
+        match (info.components, info.pixel_density) {
+            (1, 8) => pixels = grayscale_to_rgb(pixels),
+            (3, 8) => (),
+            (components, density) => {
+                bail!("Type with {components} components and {density} pixel density not supported")
+            }
+        };
+
+        Ok(Image { data: pixels, width: info.width as u32, height: info.height as u32, color_type: ColorType::RGB8 })
     }
 }
